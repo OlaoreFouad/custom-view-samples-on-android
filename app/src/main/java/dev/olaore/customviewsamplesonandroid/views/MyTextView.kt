506f5f0fadc105f5529ecf7b78120b59bdd8e272
Foodie <olaorefouad565@gmail.com>
2020-05-24 21:16:04 +0100

    feature: tried working with text on canvas

---
+++ app/src/main/java/dev/olaore/customviewsamplesonandroid/views/MyTextView.kt
@@ -0,0 +1,44 @@
+package dev.olaore.customviewsamplesonandroid.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import dev.olaore.customviewsamplesonandroid.R
+
+/**
+ * TODO: document your custom view class.
+ */
+class MyTextView @JvmOverloads
+    constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyleAttr: Int = 0)
+    : View(ctx, attributeSet, defStyleAttr) {
+
+    var textSize = 20f
+    var text = "This is some text"
+    var textPaint = Paint().apply {
+        textAlign = Paint.Align.CENTER
+        textSize = toDp(textSize).toFloat()
+        color = Color.RED
+        isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawText(text, 20f, 20f, textPaint)
+
+    }
+
+    private fun toDp(value: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            value,
+            resources.displayMetrics
+        ).toInt()
+    }
+}
