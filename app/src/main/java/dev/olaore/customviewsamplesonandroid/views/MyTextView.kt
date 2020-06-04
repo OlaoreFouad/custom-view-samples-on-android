34f41bda262bf39198f52bed81b99619c8253f6f
Foodie <olaorefouad565@gmail.com>
2020-06-04 22:26:27 +0100

    feature: learnt how to work with text a bit on android, also tried to draw some lines, that did not go well

--- app/src/main/java/dev/olaore/customviewsamplesonandroid/views/MyTextView.kt
+++ app/src/main/java/dev/olaore/customviewsamplesonandroid/views/MyTextView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
@@ -19,20 +20,52 @@ class MyTextView @JvmOverloads
     : View(ctx, attributeSet, defStyleAttr) {
 
     var textSize = 20f
-    var text = "This is some text"
+    var text = "Drawing Text"
     var textPaint = Paint().apply {
         textAlign = Paint.Align.CENTER
         textSize = toDp(textSize).toFloat()
-        textSkewX = 1f
         color = Color.RED
         isAntiAlias = true
     }
 
+    var lineStartX = 5f
+    var lineStartY = 5f
+    var lineStopX = 0f
+    var lineStopY = 0f
+
+    var lineSpacing = 20f
+    var linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GRAY
+        strokeWidth = 1.5f
+    }
+
+    var textBounds = Rect()
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawText(text, (width / 2).toFloat(), (height / 2).toFloat(), textPaint)
+        drawLines(canvas)
 
+        textPaint.getTextBounds(text, 0, text.length, textBounds)
+        var height = textBounds.height()
+        var width = textBounds.width()
+        canvas.drawText(text, width.toFloat() / 2, height + 10f, textPaint)
+
+    }
+
+    private fun drawLines(canvas: Canvas) {
+        lineStopX = width - 10f
+        for(cnt in 0..4) {
+            if (cnt == 3) {
+                linePaint.color = Color.BLACK
+            }
+
+            lineStartY = (cnt * lineSpacing) + 5f
+            lineStopY = (cnt * lineSpacing) + 5f
+
+            canvas.drawLine(lineStartX, lineStopX, lineStartY, lineStopY, linePaint)
+
+        }
     }
 
     private fun toDp(value: Float): Int {
