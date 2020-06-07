8a016e0d887d6fa951cd95ae6080eb295c2af436
Foodie <olaorefouad565@gmail.com>
2020-06-07 22:36:30 +0100

    feature: completed the line drawings fo rthe custom view and positioned the text, also started placing the widgets for modifying the text in the custom view

--- app/src/main/java/dev/olaore/customviewsamplesonandroid/views/MyTextView.kt
+++ app/src/main/java/dev/olaore/customviewsamplesonandroid/views/MyTextView.kt
@@ -10,6 +10,7 @@ import android.text.TextPaint
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
+import androidx.core.view.setPadding
 import dev.olaore.customviewsamplesonandroid.R
 
 /**
@@ -19,51 +20,65 @@ class MyTextView @JvmOverloads
     constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyleAttr: Int = 0)
     : View(ctx, attributeSet, defStyleAttr) {
 
-    var textSize = 20f
+//    text variables and paint
+    var myTextSize = 30f
     var text = "Drawing Text"
     var textPaint = Paint().apply {
         textAlign = Paint.Align.CENTER
-        textSize = toDp(textSize).toFloat()
-        color = Color.RED
+        textSize = toDp(myTextSize).toFloat()
+        color = Color.BLACK
         isAntiAlias = true
     }
 
-    var lineStartX = 5f
-    var lineStartY = 5f
-    var lineStopX = 0f
-    var lineStopY = 0f
+//    view variables
+    var extraPadding = 15
 
+//    line variables and paint
     var lineSpacing = 20f
     var linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.GRAY
         strokeWidth = 1.5f
     }
+    var startX = 0f
+    var startY = 0f
+    var endX = 0f
+    var endY = 0f
 
     var textBounds = Rect()
+
+    init {
+        setPadding(extraPadding, extraPadding, extraPadding, extraPadding)
+        lineSpacing = (height / 4).toFloat()
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         drawLines(canvas)
-
-        textPaint.getTextBounds(text, 0, text.length, textBounds)
-        var height = textBounds.height()
-        var width = textBounds.width()
-        canvas.drawText(text, width.toFloat() / 2, height + 10f, textPaint)
+        drawText(canvas)
 
     }
 
+    private fun drawText(canvas: Canvas) {
+        textPaint.getTextBounds(text, 0, text.length, textBounds)
+        var textHeight = textBounds.height()
+        var textWidth = textBounds.width()
+        canvas.drawText(text, (width / 2).toFloat(), (height / 2).toFloat(), textPaint)
+    }
+
     private fun drawLines(canvas: Canvas) {
-        lineStopX = width - 10f
+        startX = paddingLeft.toFloat()
+        lineSpacing = height / 4f
         for(cnt in 0..4) {
             if (cnt == 3) {
                 linePaint.color = Color.BLACK
             }
 
-            lineStartY = (cnt * lineSpacing) + 5f
-            lineStopY = (cnt * lineSpacing) + 5f
+            startY = paddingTop + (cnt * lineSpacing)
+            endX = (width - paddingLeft - paddingRight).toFloat()
+            endY = paddingTop + (cnt * lineSpacing)
 
-            canvas.drawLine(lineStartX, lineStopX, lineStartY, lineStopY, linePaint)
+            canvas.drawLine(startX, startY, endX, endY, linePaint)
 
         }
     }
